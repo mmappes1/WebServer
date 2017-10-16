816348768066994bae4aab105a970dec2b456881
Michael Anthony Mappes <mam70235@uga.edu>
2017-10-16 09:03:32 -0400

    Initial commit

---
+++ src/WebServer.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+public final class WebServer {
+    public static void main(String argv[]) throws Exception {
+        int port = 3333;
+    }
+}
+
+final class HttpRequest {
+
+}
